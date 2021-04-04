d202ee06b421cb12083ab5b1526644d1edf0d4a6
mfong121 <michael.fong121@gmail.com>
2021-04-04 14:52:20 -0400

    Added SQL statement for artist search

--- search.java
+++ search.java
@@ -1,20 +1,31 @@
 import java.sql.*;
+import java.util.*;
 
 public class search {
-	void artist(Connection conn, String artist_name){
+	void artist(Connection conn, Scanner scanner, String artist_name){
 		PreparedStatement ps = null;
 		ResultSet rSet = null;
-		String sqlStatement = "";
+		String sqlStatement = "SELECT Person.Name, Actor.Person_ID, Actor.Active_year FROM Person, Actor WHERE Actor.Person_ID = Person.Person_ID AND Person.Name =?";
 		try{
 		ps = conn.prepareStatement(sqlStatement);
+		ps.setString(1,artist_name);
+		rSet = ps.executeQuery();
+
+		
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+
+
+		try{
 		ps.close();
 		rSet.close();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
 	}
 
-	void track(Connection conn, String track_name){
+	void track(Connection conn, Scanner scanner, String track_name){
 		
 	}
 }
